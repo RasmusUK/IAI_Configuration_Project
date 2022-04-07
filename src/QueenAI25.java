c865bcd11b86bf6c5621794d37f23b4bb6469d3f
RasmusUK <rasmus.kritensen@live.dk>
2022-04-07 10:13:13 +0200

    Comments

--- src/QueenAI25.java
+++ src/QueenAI25.java
@@ -35,7 +35,7 @@ public class QueenAI25 implements IQueensLogic{
                 BDD var = bdds[i][j];
                 ArrayList<BDD> allBdds = new ArrayList<>();
 
-                //Diagonal
+                //Diagonals
                 allBdds.addAll(getBdds(i,j,1,1));
                 allBdds.addAll(getBdds(i,j,1,-1));
                 allBdds.addAll(getBdds(i,j,-1,-1));
@@ -62,7 +62,7 @@ public class QueenAI25 implements IQueensLogic{
                 //Do the actual implies
                 var = var.imp(temp);
 
-                //Add as and to bdd
+                //Add as an and to the bdd
                 if(bdd == null) bdd = var;
                 else bdd = bdd.and(var);
             }
@@ -113,28 +113,36 @@ public class QueenAI25 implements IQueensLogic{
 
     @Override
     public void insertQueen(int column, int row) {
+        //Insert the queen on the board
         board[column][row] = 1;
+        //Do the restriction of the variable to true
         bdd = restrictTrue(column,row);
+        //Update the rest of the board
         updateBoard();
     }
 
     //Do a true assignment
     private BDD restrictTrue(int column, int row){
+        //Set the variable at the position to true and restrict the bdd
         int var = column * size + row;
         return bdd.restrict(fact.ithVar(var));
     }
 
     //Do a false assignment
     private BDD restrictFalse(int column, int row){
+        //Set the variable at the position to false and restrict the bdd
         int var = column * size + row;
         return bdd.restrict(fact.nithVar(var));
     }
 
     private void updateBoard(){
+        //Update all the positions on the board
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
+                //Check if status is already set to 1 or -1 as they should not be changed
                 int status = board[i][j];
                 if(status != 0) continue;
+                //Set the updated status
                 board[i][j] = getUpdatedStatus(i,j);
             }
         }
@@ -142,10 +150,17 @@ public class QueenAI25 implements IQueensLogic{
 
     //See where a queen cannot be or must be
     private int getUpdatedStatus(int i, int j){
+        //Find out if there cannot be a queen
         var temp = restrictTrue(i,j);
+        //If the bdd is zero return -1 as you cannot win with a queen on that position
         if(temp.isZero())return -1;
+
+        //Find out if there should be a queen by doing the opposite
         temp = restrictFalse(i,j);
+        //If the bdd is zero when not inserting a queen, there must be a queen
         if(temp.isZero()) return 1;
+
+        //If nothing can be concluded - do nothing
         return 0;
     }
 }
